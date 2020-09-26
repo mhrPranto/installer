9e0fe6d7312a92bb288becfa0adfd40b94a9c3bf
Mohammad Hasan Rizvy Pranto <71618812+mhrPranto@users.noreply.github.com>
2020-09-26 20:38:18 +0600

    Add files via upload

---
+++ start.py
@@ -0,0 +1,76 @@
+#=======Color=====#
+N = '\033[0m'    # Normal Color
+Y = '\033[1;33m' # Yellow Color
+R = '\033[1;31m' # Red Color
+G = '\033[1;32m' # Green Color
+W = '\033[1;37m' # White Color
+D = '\033[1;9;31m'  # DelBold Color
+#==================#
+import os
+import sys
+import time
+os.system("clear")
+print("%s================================================%s" % (G,N))    
+print("%s--------All Tools Will Be Installed Soon--------%s" % (G,N))
+print("%s================================================%s" % (G,N))
+print('''%s
+ 
+                 *_*
+ ======================================
+ |Author: Mohammad Hasan Rizvy Pranto |
+ |Youtube: Green Whale                |
+ |Facebook: Green Whale               |
+ ====================================== %s''' % (R,N))    
+     
+                       
+print("%s================================================%s" % (G,N))
+print("%s::::::::::::Installation Started::::::::::::::::%s" % (G,G))
+print("%s================================================%s" % (G,N))
+print("%s================================================%s" % (G,N))
+time.sleep(5)
+os.system("apt update && apt upgrade -y")
+os.system("pkg install figlet")
+os.system("pkg install python -y")
+os.system("pkg install python2 -y")
+os.system("pkg install ruby -y") 
+os.system("pkg install git -y")
+os.system("pkg install screenfetch -y")
+os.system("pkg install cowsay -y")
+os.system("pkg install toilet -y")
+os.system("pkg install php -y")
+os.system("pkg install perl -y")
+os.system("pkg install nmap -y")
+os.system("pkg install openssh -y")
+os.system("pkg install bash -y")
+os.system("pkg install clang -y")
+os.system("pkg install jq -y")
+os.system("pkg install macchanger -y")
+os.system("pkg install nano -y")
+os.system("pkg install curl -y")
+os.system("pkg install tar -y")
+os.system("pkg install zip -y")
+os.system("pkg install fish -y")
+os.system("pkg install unzip -y")
+os.system("pkg install tor -y")
+os.system("pkg install unstable-repo -y")
+os.system("pkg install wget -y")
+os.system("pkg install tor -y")
+os.system("pkg install w3m -y")
+os.system("pkg install cmatrix -y")
+os.system("pkg install wcalc -y") 
+os.system("pkg install openssl")
+os.system("pkg install bmon -y")
+os.system("pkg install root-repo -y")
+os.system("pkg install x11-repo -y")
+
+print("%s========================================================%s" % (G,N))
+print("%sInstallation Done...................................%s" % (G,N))
+print("%s========================================================%s" % (G,N))
+print("")
+time.sleep(3)
+print("")
+print("%s===========================================%s" % (G,N))
+print("%sPlease Restart Your Termux%s" % (R,N))
+print("%s===========================================%s" % (G,N))
+time.sleep(2)
+os.system("exit")
